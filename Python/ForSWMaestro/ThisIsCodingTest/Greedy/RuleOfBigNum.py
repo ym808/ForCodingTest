8dc0b73415ee6fc0bf05e8c1831192ee17dc9610
ym808 <dlduekd18@naver.com>
2026-01-03 21:54:09 +0900

    Fix max finding logic

--- Python/ForSWMaestro/ThisIsCodingTest/Greedy/RuleOfBigNum.py
+++ Python/ForSWMaestro/ThisIsCodingTest/Greedy/RuleOfBigNum.py
@@ -1,22 +1,20 @@
 N, M, K = map(int, input().split())
 nums = list(map(int, input().split()))
 
-max = 0
-sec_max = 0
+nums.sort()
 
-for num in nums:
-    if num > max:
-        sec_max = max
-        max = num
+first = nums[-1]
+second = nums[-2]
 
 repeat = 1
 sum = 0
+
 for _ in range(M):
     if repeat > K:
-        sum += sec_max
+        sum += second
         repeat = 1
     else:
-        sum += max
+        sum += first
         repeat += 1
 print(sum)
         
