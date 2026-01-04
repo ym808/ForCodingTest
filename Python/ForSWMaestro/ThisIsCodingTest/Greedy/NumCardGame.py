4bf7c57987e99b4120b87e86ddf79ee310e275ba
ym808 <dlduekd18@naver.com>
2026-01-04 16:43:24 +0900

    Replace manual comparisons with min and max functions

--- Python/ForSWMaestro/ThisIsCodingTest/Greedy/NumCardGame.py
+++ Python/ForSWMaestro/ThisIsCodingTest/Greedy/NumCardGame.py
@@ -1,10 +1,9 @@
 N, M = map(int, input().split())
 
 matrix = []
-max = 0
+result = 0
 for n in range(N):
-    matrix = sorted(map(int, input().split()))
-    if matrix[0] > max:
-        max = matrix[0]
-print(max)
-
+    matrix = list(map(int, input().split()))
+    min_num = min(matrix)
+    result = max(min_num, result)
+print(result)
