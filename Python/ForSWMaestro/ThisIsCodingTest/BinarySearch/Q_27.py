b5c79d6a3f81bd966c940573921fb7d8de6ac6e0
ym808 <dlduekd18@naver.com>
2026-01-22 16:51:34 +0900

    Updqte Q_27.py

---
+++ Python/ForSWMaestro/ThisIsCodingTest/BinarySearch/Q_27.py
@@ -0,0 +1,51 @@
+import sys
+
+n, x = map(int, input().split())
+nums = list(map(int, sys.stdin.readline().rstrip().split()))
+
+def binarySearch(array, x, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+start = 0
+end = len(nums) - 1
+x_idx = binarySearch(nums, x, start, end)
+if x_idx == -1:
+    x_num = x_idx
+else:
+    left_end = x_idx - 1
+    right_start = x_idx + 1
+    left_result = -1
+    right_result = -1
+
+    while left_end >= 0:
+        left_x_idx = binarySearch(nums, x, start, left_end)
+        if left_x_idx == -1: break
+        left_end = left_x_idx - 1
+        left_result = left_x_idx    
+
+    while right_start <= end:
+        right_x_idx = binarySearch(nums, x, right_start, end)
+        if right_x_idx == -1: break
+        right_start = right_x_idx + 1
+        right_result = right_x_idx
+
+    if right_result == -1:
+        right_result = x_idx
+    if left_result == -1:
+        left_result = x_idx
+
+    x_num = right_result - left_result + 1
+
+print(x_num)
+    
+
+    
