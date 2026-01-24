52fcdc6d780e0b09d3c478dcabd8afd4c79c2818
ym808 <dlduekd18@naver.com>
2026-01-24 21:56:23 +0900

    Refactor is_install_possible function from binary search to greedy

--- Python/ForSWMaestro/ThisIsCodingTest/BinarySearch/Q_29.py
+++ Python/ForSWMaestro/ThisIsCodingTest/BinarySearch/Q_29.py
@@ -1,29 +1,13 @@
 def is_install_possible(dist, count):
     last_router_loc = house_locs[0]
-    start = 1
-
-    for _ in range(count - 1):
-        next_router_loc = -1
-        end = len(house_locs) - 1
-
-        while start <= end:
-            mid = (start + end) // 2
-
-            if house_locs[mid] == last_router_loc + dist:
-                next_router_loc = house_locs[mid]
-                temp = mid + 1
-                break
-            elif house_locs[mid] > last_router_loc + dist:
-                next_router_loc = house_locs[mid]
-                end = mid - 1
-                temp = mid + 1
-            else:
-                start = mid + 1
-        if next_router_loc == -1:
-            return False
-        last_router_loc = next_router_loc
-        start = temp
-    return True
+    count -= 1
+    for house_loc in house_locs:
+        if last_router_loc + dist <= house_loc:
+            last_router_loc = house_loc
+            count -= 1
+    if count <= 0:
+        return True
+    else: return False
         
 
 
