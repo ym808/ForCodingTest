8e46891fcd1ead03cd0083c183494fdc88809b33
ym808 <dlduekd18@naver.com>
2026-01-25 10:44:14 +0900

    Add input logic

---
+++ Python/ForSWMaestro/Programmers/BinarySearch/DeliverGoldAndSilver.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().split())
+g = list(map(int, input().split()))
+s = list(map(int, input().split()))
+w = list(map(int, input().split()))
+t = list(map(int, input().split()))
+
