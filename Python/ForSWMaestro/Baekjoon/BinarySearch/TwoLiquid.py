4c3e6efe2a1ea50666ade4b605af7be7584a3a96
ym808 <dlduekd18@naver.com>
2026-01-27 14:38:13 +0900

    Refactor to use two pointer reducing time complexity to O(N)

--- Python/ForSWMaestro/Baekjoon/BinarySearch/TwoLiquid.py
+++ Python/ForSWMaestro/Baekjoon/BinarySearch/TwoLiquid.py
@@ -3,24 +3,21 @@ nums = list(map(int, input().split()))
 nums.sort()
 min_sum = 10 ** 9 * 2
 
-for i in range(n):
-    start = i + 1
-    end = n - 1
-    while start <= end:
-        mid = (start + end) // 2
-        cur_sum = nums[i] + nums[mid]
+start = 0
+end = n - 1
+while start < end:
+    cur_sum = nums[start] + nums[end]
+    
+    if abs(cur_sum) < min_sum:
+        min_sum = abs(cur_sum)
+        min_pair = (start, end)
 
-        if cur_sum == 0:
-            min_pair = (i, mid)
-            break
-        elif cur_sum > 0:
-            end = mid - 1
-        else:
-            start = mid + 1
-
-        if abs(cur_sum) < min_sum:
-            min_sum = abs(cur_sum)
-            min_pair = (i, mid)
+    if cur_sum == 0: 
+        break
+    elif cur_sum < 0:
+        start += 1
+    else:
+        end -= 1
 
 for idx in min_pair:
     print(nums[idx], end=" ")
