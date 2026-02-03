420371bf40c23220d689fa508d42edda33a55d39
ym808 <dlduekd18@naver.com>
2026-02-03 20:53:26 +0900

    Refactor to use Top-down dp

--- Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/AntWarrior.py
+++ Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/AntWarrior.py
@@ -1,10 +1,16 @@
 n = int(input())
 storages = list(map(int, input().split()))
 
+dp_table = [-1] * n
+
 def return_foods(i):
     if i >= len(storages):
         return 0
+    if dp_table[i] != -1:
+        return dp_table[i]
     
-    return storages[i] + max(return_foods(i+2), return_foods(i+3))
+    dp_table[i] = storages[i] + max(return_foods(i+2), return_foods(i+3))
+
+    return dp_table[i]
 
 print(max(return_foods(0), return_foods(1)))
