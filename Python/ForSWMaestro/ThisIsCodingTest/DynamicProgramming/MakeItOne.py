2cd79591f5545cc0769a295faf9e8f848c25a629
ym808 <dlduekd18@naver.com>
2026-02-03 17:34:45 +0900

    Make a recursive function

--- Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/MakeItOne.py
+++ Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/MakeItOne.py
@@ -1,28 +1,24 @@
-num = int(input())
-table = [0] * 30001
-
-table[1] = 0
-table[2] = 1
-table[3] = 1
-table[4] = 2
-table[5] = 1
-
-for i in range(6, 30001):
+def find_optim(i):
+    match i:
+        case 1 | 2 | 3 | 5: 
+            return 1
+    
     prev_results = []
     if i % 5 == 0:
-        prev_result = table[i // 5]
+        prev_result = find_optim(i // 5)
         prev_results.append(prev_result)
     if i % 3 == 0:
-        prev_result = table[i // 3]
+        prev_result = find_optim(i // 3)
         prev_results.append(prev_result)
     if i % 2 == 0:
-        prev_result = table[i // 2]
+        prev_result = find_optim(i // 2)
         prev_results.append(prev_result)
         
-    prev_result = table[i - 1]
+    prev_result = find_optim(i - 1)
     prev_results.append(prev_result)
 
     min_result = min(prev_results)
-    table[i] = min_result + 1
+    return min_result + 1
 
-print(table[num])
+num = int(input())
+print(find_optim(num))
