ad594280ff0076d57dd5d1cade4a186ebac5f856
ym808 <dlduekd18@naver.com>
2026-02-04 16:29:52 +0900

    Add book and gpt version

--- Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/AntWarrior.py
+++ Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/AntWarrior.py
@@ -1,3 +1,4 @@
+# top-down version(mine)
 n = int(input())
 storages = list(map(int, input().split()))
 
@@ -14,3 +15,26 @@ def return_foods(i):
     return dp_table[i]
 
 print(return_foods(0))
+
+# bottom-up version(book)
+
+dp_table = [-1] * n
+
+dp_table[0] = storages[0]
+dp_table[1] = max(storages[0], storages[1])
+
+for i in range(2, n):
+    dp_table[i] = max(dp_table[i-1], dp_table[i-2] + storages[i])
+
+print(dp_table[n-1])
+
+# bottom-up no memory version(gpt)
+
+dp_i_minus2 = storages[0]
+dp_i_minus1 = max(storages[0], storages[1])
+
+for i in range(2, n):
+    dp_i = max(dp_i_minus1, dp_i_minus2 + storages[i])
+    dp_i_minus2, dp_i_minus1 = dp_i_minus1, dp_i
+
+print(dp_i)
