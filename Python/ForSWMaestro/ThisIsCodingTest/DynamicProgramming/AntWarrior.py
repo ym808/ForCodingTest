2cb5a3ffb1abf95b8c5ba0b9be3751b6cd2cc4a7
ym808 <dlduekd18@naver.com>
2026-02-04 15:22:44 +0900

    Refactor return_foods function to cleaner

--- Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/AntWarrior.py
+++ Python/ForSWMaestro/ThisIsCodingTest/DynamicProgramming/AntWarrior.py
@@ -9,8 +9,8 @@ def return_foods(i):
     if dp_table[i] != -1:
         return dp_table[i]
     
-    dp_table[i] = storages[i] + max(return_foods(i+2), return_foods(i+3))
+    dp_table[i] = max(storages[i] + return_foods(i+2), return_foods(i+1))
 
     return dp_table[i]
 
-print(max(return_foods(0), return_foods(1)))
+print(return_foods(0))
