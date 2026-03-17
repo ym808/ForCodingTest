f9445de7a236fd7b7dc4fa3656218bf5f9bda4eb
ym808 <dlduekd18@naver.com>
2026-03-17 16:45:59 +0900

    Refactor MulOrPlu.py to initialize result correctly and simplify conditional checks

--- Python/ForSWMaestro/ThisIsCodingTest/Greedy/MulOrPlu.py
+++ Python/ForSWMaestro/ThisIsCodingTest/Greedy/MulOrPlu.py
@@ -1,15 +1,9 @@
 nums = list(map(int, list(input())))
 
-result = 0
-
-result += nums[0]
+result = nums[0]
 
 for num in nums[1:]:
-    if result == 0:
-        result += num
-    elif num == 0:
-        result += num
-    elif num == 1:
+    if result <= 1 or num <= 1:
         result += num
     else:
         result *= num
