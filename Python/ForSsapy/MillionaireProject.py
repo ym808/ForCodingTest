e3c7bb0802424cb1a15d7e456a666141be175657
ym808 <dlduekd18@naver.com>
2026-04-20 14:54:45 +0900

    Refactor for clarity

--- Python/ForSsapy/MillionaireProject.py
+++ Python/ForSsapy/MillionaireProject.py
@@ -1,26 +1,14 @@
-import sys
-
-input = sys.stdin.readline
-
-# max 매번 찾기 vs max 한개 찾아두고 바뀔 때만 찾기
-
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
     prices = list(map(int, input().split()))
+    
+    max_price = 0
+    sum_price = 0
+    for i in range(len(prices)-1, -1, -1):
+        if max_price < prices[i]:
+            max_price = prices[i]
+        else:
+            sum_price += max_price - prices[i]
 
-    rep = len(prices)
-    profit_sum = 0
-
-    max_price = max(prices)
-    for i in range(rep):
-        if prices[i] == max_price:
-            max_price = 0
-            for j in range(i + 1, rep):
-                if max_price < prices[j]:
-                    max_price = prices[j]
-            continue
-
-        profit_sum += max_price - prices[i]
-
-    print(f"#{test_case} {profit_sum}")   
+    print(f"#{test_case} {sum_price}")
