9e9f2a616d2c787e4478c2e328c15c897c23a8e6
ym808 <dlduekd18@naver.com>
2026-04-21 10:54:58 +0900

    Refactor for clarity by using max()

--- Python/ForSsapy/View.py
+++ Python/ForSsapy/View.py
@@ -1,28 +1,12 @@
-
-
-T = int(input())
+T = 10
 for test_case in range(1, T + 1):
     N = int(input())
     heights = list(map(int, input().split()))
     view_households = 0
 
-    for i in range(2, len(heights)-2):
-        gaps = []
-        gaps.append(heights[i] - heights[i - 2])
-        gaps.append(heights[i] - heights[i - 1])
-        gaps.append(heights[i] - heights[i + 1])
-        gaps.append(heights[i] - heights[i + 2])
-        
-        # 4범위 안의 건물 높이가 같거나 높은 경우
-        no_gap = False
-        for gap in gaps:
-            if gap <= 0:
-                no_gap = True
-        
-        if no_gap: continue
-        # 4범위 안의 건물 높이가 작은 경우
-        min_gap = min(gaps)
-        view_households += min_gap
+    for i in range(2, N - 2):
+        max_height = max(heights[i-2], heights[i-1], heights[i+1], heights[i+2])
 
+        view_households += max(0, heights[i] - max_height)
+    
     print(f"#{test_case} {view_households}")
-        
