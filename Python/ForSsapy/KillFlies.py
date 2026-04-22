fd126f274351882b6cc52d4c7639c36ace4a7031
ym808 <dlduekd18@naver.com>
2026-04-22 14:04:10 +0900

    Refactor to use prefix-sum for time complexity

--- Python/ForSsapy/KillFlies.py
+++ Python/ForSsapy/KillFlies.py
@@ -1,21 +1,19 @@
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
+    prefix = [[0] * (N+1) for _ in range(N+1)]
     grid = []
     for _ in range(N):
         grid.append(list(map(int, input().split())))
 
-    max_killed_flies = 0
-    for i in range(N - M + 1):    
-        for j in range(N - M + 1):    
-            killed_flies = 0
-            for r in range(M):
-                for c in range(M):
-                    killed_flies += grid[r+i][c+j]
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            prefix[i][j] = grid[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
 
+    max_killed_flies = 0
+    for i in range(M, N+1):
+        for j in range(M, N+1):
+            killed_flies = prefix[i][j] - prefix[i-M][j] - prefix[i][j-M] + prefix[i-M][j-M]
             max_killed_flies = max(max_killed_flies, killed_flies)
 
     print(f"#{test_case} {max_killed_flies}")
-            
-
