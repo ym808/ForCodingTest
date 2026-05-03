8848889d0fa41fb514b665cf136744daf3419ed3
ym808 <dlduekd18@naver.com>
2026-05-03 11:24:47 +0900

    Refactor by using dp (0/1knapsack)

--- Python/ForSsapy/HamburgerDiet.py
+++ Python/ForSsapy/HamburgerDiet.py
@@ -4,27 +4,16 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     N, L = map(int, input().split())
-    preference = []
-    scores = []
-
-    for _ in range(N):
-        score, cal = map(int, input().split())
-        preference.append((score, cal))
-
-    # 모든 조합을 다 시도하고 제한 칼로리 미만 조합 중 가장 점수 높은 거
-
-    for i in range(1, 21):
-        combos = list(itertools.combinations(preference, i))
-
-        for combo in combos:
-            total_score = 0
-            total_calory = 0
-            for j in range(len(combo)):
-                total_score += combo[j][0]
-                total_calory += combo[j][1]
-            if total_calory <= L: scores.append(total_score)
-
-    print(f"#{test_case} {max(scores)}")
     
+    ingredients = []
+    dp = [0] * (L + 1)
 
+    for i in range(N):
+        score, calory = map(int, input().split())
+        ingredients.append((score, calory))
+
+    for score, calory in ingredients:
+        for c in range(L, calory - 1, -1):
+            dp[c] = max(dp[c], dp[ - calory] + score)
     
+    print(f"#{test_case} {dp[L]}")
