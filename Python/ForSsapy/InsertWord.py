b816e788d946cdc5b9093b4b04e32209c222fb80
ym808 <dlduekd18@naver.com>
2026-05-07 11:18:07 +0900

    Genius's shortest version

--- Python/ForSsapy/InsertWord.py
+++ Python/ForSsapy/InsertWord.py
@@ -1,41 +1,13 @@
-import copy
-
 T = int(input())
 
 for ts in range(1, T+1):
     N, K = map(int, input().split())
 
-    original_puzzle = []
-    result = 0
+    puzzle = [input().split() for _ in range(N)]
 
+    cnt = 0
     for i in range(N):
-        data = list(map(int, input().split()))
-        original_puzzle.append(data)
+        cnt += ''.join(puzzle[i]).split('0').count('1'*K)
+        cnt += ''.join(puzzle[j][i] for j in range(N)).split('0').count('1'*K)
 
-    
-    puzzle = copy.deepcopy(original_puzzle)
-    for r in range(N):
-        for c in range(0, N - K + 1):
-            if puzzle[r][c] == 0: continue
-            length = 0
-
-            for i in range(N - c):
-                nc = c + i
-                if puzzle[r][nc] == 0: break
-                length += 1
-                puzzle[r][nc] = 0
-            if length == K: result += 1
-
-    puzzle = copy.deepcopy(original_puzzle)
-    for c in range(N):
-        for r in range(0, N - K + 1):
-            if puzzle[r][c] == 0: continue
-            length = 0
-
-            for i in range(N - r):
-                nr = r + i
-                if puzzle[nr][c] == 0: break
-                length += 1
-                puzzle[nr][c] = 0
-            if length == K: result += 1
-    print(f"#{ts} {result}")
+    print(f"#{ts} {cnt}")
