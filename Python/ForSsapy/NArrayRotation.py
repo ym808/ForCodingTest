82cb27af5ffdc0a4a45b495e7f95737dd9bfef6c
ym808 <dlduekd18@naver.com>
2026-05-11 09:33:55 +0900

    Gemini ver

--- Python/ForSsapy/NArrayRotation.py
+++ Python/ForSsapy/NArrayRotation.py
@@ -1,23 +1,16 @@
-import copy
-T = 1 #int(input())
+T = int(input())
 
 for ts in range(1, T+1):
     N = int(input())
     grid = []
-    rotated_grids = []
+
     for _ in range(N):
-        row = input().split()
-        grid.append(row)
+        grid.append(input().split())
     
-    def turn(arr, N):
-        result1 = ""
-        result2 = ""
-        result3 = ""
-        for i in range(N):
-            for j in range(N):
-            # print(i, j)
-                result1 += arr[N-j-1][i] # 90도
-                result2 += arr[N-i-1][N-j-1] # 180도
-                result3 += arr[j][N-i-1] # 180도
-                print(result1, result2, result3)
-                result1, result2, result3 = "", "", ""
+    print(f"#{ts} ")
+    for i in range(N):
+        rot90 = ''.join(grid[N-1-j][i] for j in range(N))
+        rot180 = ''.join(grid[N-1-i][N-1-j] for j in range(N))
+        rot270 = ''.join(grid[j][N-1-i] for j in range(N))
+
+        print(rot90, rot180, rot270)
